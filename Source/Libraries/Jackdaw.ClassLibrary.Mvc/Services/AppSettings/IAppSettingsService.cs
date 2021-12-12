878eeec1b960d1b0f19fc5ca8d534c9239dd0c0c
Christopher Cavell <cdcavell@gmail.com>
2021-12-12 17:03:22 -0600

    0.0.0.1 - Initial Development

---
+++ Source/Libraries/Jackdaw.ClassLibrary.Mvc/Services/AppSettings/IAppSettingsService.cs
@@ -0,0 +1,63 @@
+﻿namespace Jackdaw.ClassLibrary.Mvc.Services.AppSettings
+{
+    /// <summary>
+    /// AppSettings Web Service Interface
+    /// </summary>
+    /// <revision>
+    /// __Revisions:__~~
+    /// | Contributor | Build | Revison Date | Description |~
+    /// |-------------|-------|--------------|-------------|~
+    /// | Christopher D. Cavell | 0.0.0.1 | 12/12/2021 | Initial Development |~ 
+    /// </revision>
+    public interface IAppSettingsService
+    {
+        /// <summary>
+        /// Get AssemblyName value
+        /// </summary>
+        /// <returns>string</returns>
+        public string AssemblyName();
+
+        /// <summary>
+        /// Get AssemblyVersion value
+        /// </summary>
+        /// <returns>string</returns>
+        public string AssemblyVersion();
+
+        /// <summary>
+        /// Get LastModifiedDate value
+        /// </summary>
+        /// <returns>string</returns>
+        public string LastModifiedDate();
+
+        /// <summary>
+        /// Get EnvironmentName value
+        /// </summary>
+        /// <returns>string</returns>
+        public string EnvironmentName();
+
+        /// <summary>
+        /// Get IsDevelopment value
+        /// </summary>
+        /// <returns>bool</returns>
+        public bool IsDevelopment();
+
+        /// <summary>
+        /// Get IsProduction value
+        /// </summary>
+        /// <returns>bool</returns>
+        public bool IsProduction();
+
+        /// <summary>
+        /// Get raw json string value
+        /// </summary>
+        /// <returns>string</returns>
+        public string ToJson();
+
+        /// <summary>
+        /// Get dynamic object
+        /// </summary>
+        /// <returns>T?</returns>
+        /// <method>public T ToObject&lt;T&gt;()</method>
+        public T ToObject<T>();
+    }
+}
---
+++ Source/Libraries/Jackdaw.ClassLibrary.Mvc/Services/AppSettings/Models/AppSettings.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Reflection;
+
+namespace Jackdaw.ClassLibrary.Mvc.Services.AppSettings.Models
+{
+    /// <summary>
+    /// Application settings information.
+    /// </summary>
+    /// <revision>
+    /// __Revisions:__~~
+    /// | Contributor | Build | Revison Date | Description |~
+    /// |-------------|-------|--------------|-------------|~
+    /// | Christopher D. Cavell | 0.0.0.1 | 12/12/2021 | Initial Development |~ 
+    /// </revision>
+    public abstract class AppSettings
+    {
+        private static readonly string _assemblyName = Assembly.GetEntryAssembly()?.GetName()?.Name ?? String.Empty;
+        private static readonly string _assemblyVersion = Assembly.GetEntryAssembly()?.GetName()?.Version?.ToString() ?? String.Empty;
+        private static readonly string _assemblyLocation = Assembly.GetEntryAssembly()?.Location ?? String.Empty;
+        private static readonly string _environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? String.Empty;
+        private static readonly DateTime _lastModifiedDateTime = String.IsNullOrEmpty(_assemblyLocation) ? DateTime.MinValue : File.GetLastWriteTime(_assemblyLocation);
+
+        /// <summary>
+        /// Contstructor 
+        /// </summary>
+        /// <param name="configuration">IConfiguration</param>
+        protected AppSettings(IConfiguration configuration)
+        {
+            if (configuration != null)
+                configuration.Bind("AppSettings", this);
+        }
+
+        /// <value>string</value>
+        public string AssemblyName { get; } = _assemblyName;
+
+        /// <value>string</value>
+        public string AssemblyVersion { get; } = _assemblyVersion;
+
+        /// <value>string</value>
+        public string EnvironmentName { get; } = _environmentName;
+
+        /// <value>bool</value>
+        public bool IsDevelopment { get; } = _environmentName.Equals("Development", StringComparison.OrdinalIgnoreCase);
+
+        /// <value>bool</value>
+        public bool IsProduction { get; } = _environmentName.Equals("Production", StringComparison.OrdinalIgnoreCase);
+
+        /// <value>DateTime</value>
+        public DateTime LastModifiedDateTime { get; } = _lastModifiedDateTime;
+    }
+}
