a0e36e540a259073301b52d8e6fc1dc0193f4de6
Christopher Cavell <cdcavell@gmail.com>
2021-12-12 11:00:00 -0600

    0.0.0.1 - Initial Development

--- Source/Web/Jackdaw.Public/Program.cs
+++ Source/Web/Jackdaw.Public/Program.cs
@@ -1,6 +1,48 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Serilog;
 
-app.MapGet("/", () => "Hello World!");
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-app.Run();
+Log.Information("Starting Host");
+
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+    builder.Host.UseSerilog((ctx, lc) => lc
+        .ReadFrom.Configuration(ctx.Configuration));
+
+    Log.Information("Adding services to the container");
+
+    builder.Services.AddHttpContextAccessor();
+    builder.Services.AddControllersWithViews();
+
+    var app = builder.Build();
+    Log.Information("Configuring the HTTP request pipeline");
+
+    app.UseSerilogRequestLogging();
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}"
+    );
+
+    Log.Information("Starting application");
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Unhandled exception");
+}
+finally
+{
+    Log.Information("Shut down complete");
+    Log.CloseAndFlush();
+}
