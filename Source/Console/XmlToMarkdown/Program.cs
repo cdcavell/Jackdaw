a9c1c803b0ec27ac9248aea09e71c3b5f0140716
Christopher Cavell <cdcavell@gmail.com>
2021-12-11 18:05:52 -0600

    0.0.0.1 - Initial Development

--- Source/Console/XmlToMarkdown/Program.cs
+++ Source/Console/XmlToMarkdown/Program.cs
@@ -14,7 +14,7 @@ namespace XmlToMarkdown
     /// __Revisions:__~~
     /// | Contributor | Build | Revison Date | Description |~
     /// |-------------|-------|--------------|-------------|~
-    /// | Christopher D. Cavell | 0.0.0.1 | 10/16/2021 | Initial Development |~ 
+    /// | Christopher D. Cavell | 0.0.0.1 | 12/11/2021 | Initial Development |~ 
     /// </revision>
     class Program
     {
