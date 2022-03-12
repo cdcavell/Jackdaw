f99ee166127751d8df8f157e5b316ebf7458db97
Christopher Cavell <cdcavell@gmail.com>
2022-03-12 16:34:25 -0600

    0.0.0.2 - Duende IdentityServer Integration

---
+++ Source/Web/Jackdaw.IdentityServer/Models/Device/DeviceAuthorizationInputModel.cs
@@ -0,0 +1,22 @@
+﻿using Jackdaw.IdentityServer.Models.Consent;
+
+namespace Jackdaw.IdentityServer.Models.Device
+{
+    /// <summary>
+    /// Device Authorization Input Model
+    /// &lt;br /&gt;&lt;br /&gt;
+    /// Copyright (c) Duende Software. All rights reserved.
+    /// See https://duendesoftware.com/license/identityserver.pdf for license information. 
+    /// </summary>
+    /// <revision>
+    /// __Revisions:__~~
+    /// | Contributor | Build | Revison Date | Description |~
+    /// |-------------|-------|--------------|-------------|~
+    /// | Christopher D. Cavell | 0.0.0.2 | 03/12/2022 | Duende IdentityServer Integration |~ 
+    /// </revision>
+    public class DeviceAuthorizationInputModel : ConsentInputModel
+    {
+        /// <value>string</value>
+        public string? UserCode { get; set; }
+    }
+}
