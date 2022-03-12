8d45c37f11854856929f1bc4e805c24aac5558ce
Christopher Cavell <cdcavell@gmail.com>
2022-03-12 17:24:36 -0600

    0.0.0.2 - Duende IdentityServer Integration

---
+++ Source/Web/Jackdaw.IdentityServer/Models/Grants/GrantViewModel.cs
@@ -0,0 +1,36 @@
+﻿namespace Jackdaw.IdentityServer.Models.Grants
+{
+    /// <summary>
+    /// Grant View Model
+    /// &lt;br /&gt;&lt;br /&gt;
+    /// Copyright (c) Duende Software. All rights reserved.
+    /// See https://duendesoftware.com/license/identityserver.pdf for license information. 
+    /// </summary>
+    /// <revision>
+    /// __Revisions:__~~
+    /// | Contributor | Build | Revison Date | Description |~
+    /// |-------------|-------|--------------|-------------|~
+    /// | Christopher D. Cavell | 0.0.0.2 | 03/12/2022 | Duende IdentityServer Integration |~ 
+    /// </revision>
+    public class GrantViewModel
+    {
+        /// <value>string</value>
+        public string? ClientId { get; set; }
+        /// <value>string</value>
+        public string? ClientName { get; set; }
+        /// <value>string</value>
+        public string? ClientUrl { get; set; }
+        /// <value>string</value>
+        public string? ClientLogoUrl { get; set; }
+        /// <value>string</value>
+        public string? Description { get; set; }
+        /// <value>DateTime</value>
+        public DateTime Created { get; set; }
+        /// <value>DateTime?</value>
+        public DateTime? Expires { get; set; }
+        /// <value>IEnumerable&lt;string&gt;</value>
+        public IEnumerable<string> IdentityGrantNames { get; set; } = new List<string>();
+        /// <value>IEnumerable&lt;string&gt;</value>
+        public IEnumerable<string> ApiGrantNames { get; set; } = new List<string>();
+    }
+}
---
+++ Source/Web/Jackdaw.IdentityServer/Models/Grants/GrantsViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Jackdaw.IdentityServer.Models.Grants
+{
+    /// <summary>
+    /// Grants View Model
+    /// &lt;br /&gt;&lt;br /&gt;
+    /// Copyright (c) Duende Software. All rights reserved.
+    /// See https://duendesoftware.com/license/identityserver.pdf for license information. 
+    /// </summary>
+    /// <revision>
+    /// __Revisions:__~~
+    /// | Contributor | Build | Revison Date | Description |~
+    /// |-------------|-------|--------------|-------------|~
+    /// | Christopher D. Cavell | 0.0.0.2 | 03/12/2022 | Duende IdentityServer Integration |~ 
+    /// </revision>
+    public class GrantsViewModel
+    {
+        /// <value>IEnumerable&lt;GrantViewModel&gt;</value>
+        public IEnumerable<GrantViewModel> Grants { get; set; } = new List<GrantViewModel>();
+    }
+}
